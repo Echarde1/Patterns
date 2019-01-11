41d931fbd2d42a3aa6d28889682904e87c73c9aa
novoselov <novoselov@bifit.com>
2019-01-11 17:53:27 +0300

    6 chapter. Time to finish it

---
+++ 6_Chapter_CommandPattern/remote/src/GarageDoorUpCommand.java
@@ -0,0 +1,12 @@
+public class GarageDoorUpCommand implements Command {
+    GarageDoor door;
+
+    public GarageDoorUpCommand(GarageDoor door) {
+        this.door = door;
+    }
+
+    @Override
+    public void execute() {
+        door.up();
+    }
+}
---
+++ 6_Chapter_CommandPattern/remote/src/RemoteControlTest.java
@@ -0,0 +1,51 @@
+public class RemoteControlTest {
+    public static void main(String[] args) {
+        RemoteControl remoteControl = new RemoteControl();
+
+        Light lightLivingRoom = new Light("Living Room");
+        Light lightKitchen = new Light("Kitchen");
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+        GarageDoor garageDoor = new GarageDoor("");
+        Stereo stereo = new Stereo("Living Room");
+
+        LightOnCommand livingRoomLightOn =
+                new LightOnCommand(lightLivingRoom);
+        LightOffCommand livingRoomLightOff =
+                new LightOffCommand(lightLivingRoom);
+        LightOnCommand kitchenLightOn =
+                new LightOnCommand(lightKitchen);
+        LightOffCommand kitchenLightOff =
+                new LightOffCommand(lightKitchen);
+
+        CeilingFanOnCommand ceilingFanOn =
+                new CeilingFanOnCommand(ceilingFan);
+        CeilingFanOffCommand ceilingFanOff =
+                new CeilingFanOffCommand(ceilingFan);
+
+        GarageDoorUpCommand garageDoorUp =
+                new GarageDoorUpCommand(garageDoor);
+        GarageDoorDownCommand garageDoorDown =
+                new GarageDoorDownCommand(garageDoor);
+
+        StereoOnWithCDCommand stereoOnWithCD =
+                new StereoOnWithCDCommand(stereo);
+        StereoOffCommand  stereoOff =
+                new StereoOffCommand(stereo);
+
+        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+        remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
+        remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+
+        System.out.println(remoteControl);
+
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+        remoteControl.onButtonWasPushed(1);
+        remoteControl.offButtonWasPushed(1);
+        remoteControl.onButtonWasPushed(2);
+        remoteControl.offButtonWasPushed(2);
+        remoteControl.onButtonWasPushed(3);
+        remoteControl.offButtonWasPushed(3);
+    }
+}
